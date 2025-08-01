bea57f30cc062248b33f27ccf39698d84f6dd02a
Charles Khoza <khozacharles832@gmail.com>
2025-08-01 11:38:31 +0200

    style auth pages

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -1,7 +1,7 @@
 import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
 import { Text, View } from 'react-native'
-import { SignOutButton } from '@/components/SignOutButton'
+import { SignOutButton } from "@/components/signOutButton";
 
 export default function Page() {
   const { user } = useUser()
